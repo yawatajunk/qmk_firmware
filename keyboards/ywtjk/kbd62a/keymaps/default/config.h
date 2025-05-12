5b587674590a295b46effd29c786ba704e224921
Jun <jun@80k.xyz>
2025-05-12 21:17:39 +0900

    2025/05/12: ywtjk/kbd62s(rp2040による62キースプリッドキーボード)の開発スタート。ファームウェアをコンパイル成功。動作未検証

--- keyboards/ywtjk/kbd62a/keymaps/default/config.h
+++ keyboards/ywtjk/kbd62a/keymaps/default/config.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 3
-
 // ダブルタップリセットLED
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP0 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+//#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP0 // Specify a optional status led by GPIO number which blinks when entering the bootloader
