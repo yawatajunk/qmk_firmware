36b921ef535629fe41100f6193d9e8278e031fd9
Jun <jun@80k.xyz>
2025-05-17 15:45:10 +0900

    2025/05/17: OLEDの表示に成功

--- keyboards/ywtjk/kbd62a/keymaps/default/config.h
+++ keyboards/ywtjk/kbd62a/keymaps/default/config.h
@@ -4,3 +4,9 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U // Timeout window in ms in which the double tap can occur.
 //#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP0 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
+// OLED
+#define OLED_DISPLAY_128X64
+#define OLED_TIMEOUT 0
+#define I2C1_SDA_PIN GP14
+#define I2C1_SCL_PIN GP15
--- keyboards/ywtjk/kbd62a/keymaps/default/keymap.c
+++ keyboards/ywtjk/kbd62a/keymaps/default/keymap.c
@@ -6,8 +6,8 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT (
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     MO(1),    MO(1),    KC_6   ,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_ESC,   KC_BSLS,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_EQL,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     MO(1),    UG_NEXT,  KC_6   ,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_ESC,   MO(1),    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_EQL,
         KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_LBRC,  KC_RBRC,  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                         KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
         MO(1),    KC_LOPT,  MOD_LCTL, MOD_LGUI, KC_SPC,                       KC_ENT,   MOD_LGUI, KC_BSPC,  MO(1)
@@ -21,3 +21,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 // clang-format on
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write(PSTR("QWERTY\n"), false);
+            break;
+        case 1:
+            oled_write(PSTR("FN\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
